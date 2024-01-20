95b26227ce603177a295fe352f3263a65c598166
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2024-01-20 12:19:53 +0100

    Adding routes and components

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,24 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { colors } from "../../constants/colors";
+import { useState } from "react";
+import { Category } from "../";
+
+const Main = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  return (
+    <Stack>
+      <Category />
+      <Box p={2} sx={{ height: "90vh" }}>
+        <Container maxWidth={"90%"}>
+          <Typography variant={"h4"} fontWeight={"bold"} mb={2}>
+            {selectedCategory}{" "}
+            <span style={{ color: colors.secondary }}>videos</span>
+          </Typography>
+          Videos
+        </Container>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Main;
